42709b01361138bd38b49944f62d5abc79338239
Nitish Deshmukh <nitishdeshmukh24@gmail.com>
2025-11-05 00:22:21 +0530

    Update db.js to include database name from environment variable

--- food-del/backend/config/db.js
+++ food-del/backend/config/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   const uri = process.env.MONGO_URI;
+  const dbName = process.env.DB_NAME;
   if (!uri) {
     throw new Error("MONGO_URI not set");
   }
-  await mongoose.connect(uri);
+  await mongoose.connect(`${uri}/${process.env.DB_NAME}`);
   console.log("DB Connected");
 };
